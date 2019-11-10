12f76029b0a079d7566f8a928258a2210197c7e7
m4dm4tt <mridulmathew@gmail.com>
2019-11-10 23:27:07 +0530

    Changes to query parser for acceldata

--- query_database.sql
+++ query_database.sql
@@ -2,6 +2,57 @@ SELECT
   job_id, 
   AVG(salary) 
 FROM 
+  employees e, dept d 
+WHERE 
+  e.dept_id = d.dept_id 
+  AND 1 = 1 
+  AND e.salary BETWEEN (10000 + 1) AND 100000 * 10 
+  AND e.bonus BETWEEN 100 + 2 AND (1000 * 15) 
+GROUP BY 
+  job_id 
+HAVING 
+  AVG(salary)< (
+    SELECT 
+      MAX(
+        AVG(j.min_salary)
+      ) 
+    FROM 
+      (
+        select 
+          * 
+        from 
+          jobs
+      ) as j 
+    WHERE 
+      j.job_id IN (
+        SELECT 
+          jh.job_id 
+        FROM 
+          job_history jh FULL OUTER JOIN job_history_archive jha 
+        WHERE jh.hist_id = jha.hist_id 
+          AND jh.department_id BETWEEN 50 AND 100 
+        ORDER BY 
+          jh.job_id
+      ) 
+      AND j.job_id IN (
+        SELECT 
+          global_job_type 
+        FROM 
+          params
+      ) 
+    GROUP BY 
+      j.job_id
+  ) 
+ORDER BY 
+  d.dept_id 
+LIMIT 
+  10;
+
+___________________________________________________________
+SELECT 
+  job_id, 
+  AVG(salary) 
+FROM 
   employees e, 
   (
     select 
@@ -128,6 +179,7 @@ ORDER BY
 LIMIT 
   10;
 
+___________________________________________________________
 
 select 
        nvl(a, b) 
@@ -160,3 +212,26 @@ where
     where 
       t1.a = t5.b
   );
+  
+___________________________________________________________  
+
+select 
+  * 
+from 
+  (
+    SELECT 
+      v.name, 
+      c.name, 
+      p.lastname 
+    FROM 
+      vehicle v 
+      INNER JOIN color c ON v.color_id = c.id 
+      INNER JOIN person p ON v.person_id = p.id
+  ) abc 
+ORDER BY 
+  name 
+LIMIT 
+  10;
+
+
+___________________________________________________________
--- subq_main.py
+++ subq_main.py
@@ -2,7 +2,8 @@
 
 __author__      = "Mridul Mathew"
 __copyright__   = "Copyright 2019, mridulmathew@gmail.com"
-__assumptions__ = "The SQL construct is well formed. E.g., An SQL query should always end with a ';' or '/'"
+__assumptions__ = "The SQL construct is well formed. E.g., A SQL query should always end with a ';' or '/'"
+__known_bugs__ = "(1) Will not print full table list for multiple ANSI joins (2) Will not print the last closing bracket of the second expression of between clauses."
 
 import os
 from os import path
@@ -12,9 +13,6 @@ from collections import deque
 import subq_lib
 subq_start_pos_array = []
 subq_end_pos_array = []
-g_all_tokens = []
-g_subq_start_tokens = []
-g_subq_end_tokens = []
 g_strsql = ''
 	
 
@@ -111,7 +109,6 @@ def find_all_bracket_pos():
 def find_clauses_start_end_pos():
 	l_between_clause_pairs = []
 	l_end_pos = len(g_all_tokens)
-	write_debug_out("Between clause pairs: ")
 	for l_index,l_token in enumerate(g_all_tokens):
 		if str(g_all_tokens[l_index]).lower() == 'between':
 			l_between_pos = l_index
@@ -124,9 +121,9 @@ def find_clauses_start_end_pos():
 					l_between_clause_end_pos = m-1
 					break
 			l_between_clause_pairs.append([l_between_pos, l_between_clause_end_pos])
+	write_debug_out("Between clause positions: ")
 	write_debug_out(l_between_clause_pairs)
 	l_order_by_clause_pairs = []
-	write_debug_out("Order by clause pairs: ")
 	for l_index,l_token in enumerate(g_all_tokens):
 		if (str(l_token).lower() == 'order by') :
 			l_order_by_pos = l_index
@@ -135,49 +132,59 @@ def find_clauses_start_end_pos():
 					l_order_by_end_pos = m-1
 					break
 			l_order_by_clause_pairs.append([l_order_by_pos, l_order_by_end_pos])
+	write_debug_out("Order by clause positions: ")
 	write_debug_out(l_order_by_clause_pairs)
 	l_join_clause_pairs = []
-	write_debug_out("Join clause pairs: ")
 	for l_index,l_token in enumerate(g_all_tokens):
 		if (str(l_token).lower() in subq_lib.joins_tokens_array) :
+			l_join_type = l_token
 			l_join_pos = l_index
-			print("\t"+"JOIN TYPE: "+l_token)
-			print(l_join_pos)
-					
-
-	return(l_between_clause_pairs, l_order_by_clause_pairs)
-
-def find_join_clauses(l_start_pos, l_end_pos):
-	l_end_pos = len(g_all_tokens)
-	write_debug_out("JOIN clauses: ")
-	for l_index,l_token in enumerate(g_all_tokens):
-		if str(g_all_tokens[l_index]).lower() == 'where':
-			l_between_pos = l_index
-			for k in range(l_between_pos+1, l_end_pos):
-				if str(g_all_tokens[k]).lower() == 'and':
-					l_separator_and_pos = k
+			for k in range(l_join_pos, -1, -1):
+				if (str(g_all_tokens[k]).lower() == 'from') :
+					l_join_start = k+1
 					break
-			for m in range(l_separator_and_pos+1, l_end_pos):
-				if str(g_all_tokens[m]).lower() in subq_lib.between_clause_end_array:
-					l_between_clause_end_pos = m-1
+			for m in range(l_join_pos, l_end_pos+1):
+				if (g_all_tokens[m]).lower() in subq_lib.join_clause_end_array:
+					l_join_end = m-1
 					break
-			l_between_clause_pairs.append([l_between_pos, l_between_clause_end_pos])
-	write_debug_out(l_between_clause_pairs)
+			l_join_clause_pairs.append([l_join_start, l_join_end, l_join_type])	
+			break
+	write_debug_out("Join clause positions: ")
+	write_debug_out(l_join_clause_pairs)					
+
+	return(l_between_clause_pairs, l_order_by_clause_pairs, l_join_clause_pairs)
+
+def print_join_list(l_start_pos, l_end_pos):
+	# Purpose: To print the join clauses and their types.
+	l_join_str = "\t"+"JOIN CLAUSE: "
+	for k in g_join_clause_pairs:
+		if (k[0] >= l_start_pos) and (k[1] <= l_end_pos):
+			n1=k[0]
+			n2=k[1]
+			for m in range(n1, n2+1):
+				l_join_str = l_join_str + " " + g_all_tokens[m]
+			print(l_join_str)
+			print("\t"+"JOIN TYPE: "+k[2])			
 
 def print_table_list(l_start_pos, l_end_pos):
 	# Purpose: To print the list of tables inside subqueries along with their aliases.
-	print('\t'+ "TABLE list along with ALIAS: " )
 	for k in range(l_start_pos, l_end_pos+1):
 		if str(g_all_tokens[k]).lower() == 'from':
 			l_from_pos = k
 			break
 	l_table_list = '\t'
 	for j in range(l_from_pos+1, l_end_pos):
-		if (str(g_all_tokens[j]).lower() == 'where') or (str(g_all_tokens[j]).lower() == 'on') (l_from_pos+1 == l_end_pos):
+		#if (str(g_all_tokens[j]).lower() == 'where') or ((str(g_all_tokens[j]).lower() == 'on')) or (l_from_pos+1 == l_end_pos) or (g_all_tokens[j] =';'):
+		if (str(g_all_tokens[j]).lower() == 'where') or ((str(g_all_tokens[j]).lower() == 'on')) or (l_from_pos+1 == l_end_pos) or (g_all_tokens[j] ==';'):
 			break
+			#?? bug. what about multiple string of joins
+			# SELECT v.name , c.name , p.lastname FROM vehicle v INNER JOIN color c ON v.color_id = c.id INNER JOIN person p ON v.person_id = p.id
 		else:
-			l_table_list = l_table_list + " " + g_all_tokens[j]		
-	print(l_table_list)
+			if str(g_all_tokens[j]).lower() in subq_lib.joins_tokens_array:
+				l_table_list = l_table_list + ","		
+			else:
+				l_table_list = l_table_list + " " + g_all_tokens[j]		
+	print('\t'+ "TABLE list along with ALIAS: " + l_table_list )
 
 def print_subqs():
 	# Purpose: To print the sub-queries.
@@ -185,14 +192,19 @@ def print_subqs():
 	print("Number of SUB QUERIES : " + str(len(g_subq_bracket_pairs)))
 	print('\n')
 	print("SUB QUERY list: " )
+	l_subq_counter = 1;
 	for i in g_subq_bracket_pairs:
 		l_subquery = ''
 		for k in range(i[0], i[1]+1):
 			l_subquery = l_subquery + ' ' + str(g_all_tokens[k])
-		print(l_subquery)		
+			
+		print(str(l_subq_counter) + ".")
+		print("\t"+"SUB QUERY TEXT:" + l_subquery)		
+		l_subq_counter = l_subq_counter + 1
 		if (i[2] != ''):
-			print("\t"+"SUB QUERY ALIAS:" + i[2])
+			print("\t"+"SUB QUERY ALIAS: " + i[2])
 		print_table_list(i[0], i[1])
+		print_join_list(i[0], i[1])
 		print('-' * 100)
 
 def print_between_clauses():
@@ -226,7 +238,7 @@ def print_limit_clause():
 	for k in range(len(g_all_tokens)-1, len(g_all_tokens)-4, -1):
 		if (g_all_tokens[k] != ';' and g_all_tokens[k] != '/'):
 			l_limit_clause =   g_all_tokens[k] + " " + l_limit_clause 
-		if str(g_all_tokens[k]).lower() != 'limit':
+		if str(g_all_tokens[k]).lower() == 'limit':
 			l_limit_exists = 1
 	if (l_limit_exists == 1):
 		print(("\t" + l_limit_clause))
@@ -237,8 +249,8 @@ if __name__== "__main__":
 	welcome()
 	g_strsql, g_all_tokens = parse_infile('myquery.txt')
 	g_all_bracket_pairs, g_subq_bracket_pairs, g_subq_start_tokens = find_all_bracket_pos()
-	g_between_clause_pairs, g_order_by_clause_pairs =  find_clauses_start_end_pos()
-#	print_subqs()
-#	print_limit_clause()
-#	print_between_clauses()
-#	print_orderby_clauses()
+	g_between_clause_pairs, g_order_by_clause_pairs, g_join_clause_pairs =  find_clauses_start_end_pos()
+	print_subqs()
+	print_limit_clause()
+	print_between_clauses()
+	print_orderby_clauses()
